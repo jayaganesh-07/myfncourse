099564e72666bd1af363cc9205a7a55728b404ee
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-28 12:02:24 +0530

    updated

---
+++ MYSQL/Day4/Employees.sql
@@ -0,0 +1,39 @@
+
+USE Employees;
+
+CREATE TABLE Employe (
+    id INT,
+    name VARCHAR(100),
+    department VARCHAR(50),
+    salary INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO Employe (id, name, department, salary, city)
+VALUES
+(101, 'Sudhan', 'Developer', 65000, 'Chennai'),
+(102, 'Rahul', 'Tester', 40000, 'Madurai'),
+(103, 'Karthik', 'Developer', 55000, 'Chennai'),
+(104, 'Ajay', 'HR', 35000, 'Salem'),
+(105, 'Vijay', 'Manager', 85000, 'Coimbatore'),
+(106, 'Arun', 'Developer', 60000, 'Bangalore'),
+(107, 'Priya', 'Tester', 45000, 'Chennai'),
+(108, 'Divya', 'HR', 30000, 'Madurai'),
+(109, 'Surya', 'Developer', 70000, 'Salem'),
+(110, 'Anitha', 'Manager', 90000, 'Chennai');
+-- task 1
+SELECT * FROM Employe;
+-- task 2
+SELECT name,department,salary FROM Employe;
+-- task 3
+SELECT  distinct city from Employe;
+-- task 4
+SELECT * FROM Employe WHERE city = "chennai" ;
+-- task 5
+SELECT * FROM Employe WHERE salary <= 45000;
+-- task 6 (1)
+SELECT * FROM Employe WHERE department= 'developer' AND city = 'chennai';
+   -- (2)
+SELECT * FROM Employe WHERE  city= 'Madurai' OR city = 'salem';
+-- task 7
+SELECT * FROM Employe WHERE city LIKE  'A%';
